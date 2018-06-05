3e13f47b43b26e80e8cbb8f3601c88c9acce6125
SinaGudbrandsen <sinahgudb@gmail.com>
2018-06-05 19:14:40 +0200

    Removing old code. Giving each circle its coundown and removing buttons

--- www/script.js
+++ www/script.js
@@ -2,76 +2,166 @@
 
 function outputname() {
     
-    // name input
-    
+// name input
     var waterIt = document.getElementById('form1'); 
-    
     var y = waterIt.elements['addNameOne'].value;  
-    
     console.log(y); 
 
+// name input to become header
     document.querySelector('.titleOne').innerHTML = y; 
+
+// remove 'start watering' button
+    var removeButton = document.querySelector('.startWateringOne');
+    removeButton.classList.remove('startWateringOne'); 
+    removeButton.classList.add('startWateringOneOff'); 
     
-    
-    var counter = 0; 
-    var timeLeft = 604800; 
+// start coundown
+    var counterOne = 0; 
+    var timeLeftOne = 604800; 
 
             function timeIt(){
     
-                counter++; 
-                console.log(timeLeft - counter);   
+                counterOne++; 
+                console.log(timeLeftOne - counterOne);
+                
+                if (counterOne == timeLeftOne){
+
+                    counterOne = 0; 
+
+// add circle when counter has hit 0
+                    var addclassWrapper = document.querySelector('.timeToWaterNone');
+                        addclassWrapper.classList.remove('timeToWaterNone');
+                        addclassWrapper.classList.add('timeToWater');
+                }
                       
     }
     setInterval(timeIt, 1000); 
 } 
     
 // function start watering on circle number two
-
 function outputnametwo() {
 
-    //name input
-
+ //name input
     var name = document.getElementById('form2'); 
-    
     var y = name.elements["nametwo"].value;  
-    
     console.log(y); 
 
+
+// name input to become header
     document.querySelector('.titleTwo').innerHTML = y;
 
-    var counter = 0; 
-    var timeLeft = 1209600; 
+// remove start watering button
+    var removeButton = document.querySelector('.startWateringTwo');
+    removeButton.classList.remove('startWateringTwo'); 
+    removeButton.classList.add('startWateringTwoOff'); 
+
+// start countdown
+    var counterTwo = 0; 
+    var timeLeftTwo = 1209600; 
 
         function timeIt(){
 
-            counter++; 
-            console.log(timeLeft - counter); 
+            counterTwo++; 
+            console.log(timeLeftTwo - counterTwo); 
+
+            if (counterTwo == timeLeftTwo){
+
+                counterTwo = 0; 
+
+// add circle when counter has hit 0
+                var addclassWrapper = document.querySelector('.timeToWaterTwoNone');
+                    addclassWrapper.classList.remove('timeToWaterTwoNone');
+                    addclassWrapper.classList.add('timeToWaterTwo');
+        
+            }
     }
             
     setInterval(timeIt, 1000); 
 
 }   
 
-
+// function start watering on circle number two
 function outputnamethree() {
-
+// name input
     var namethree = document.getElementById('form3'); 
-
     var t = namethree.elements["namethree"].value; 
-
     console.log(t); 
 
+// name input to become header
     document.querySelector('.titleThree').innerHTML = t;
 
-    var counter = 0; 
-    var timeLeft = 302400; 
+// remove start watering button
+    var removeButton = document.querySelector('.startWateringThree');
+    removeButton.classList.remove('startWateringThree'); 
+    removeButton.classList.add('startWateringThreeOff'); 
+    
+// start countdown
+    var counterThree = 0; 
+    var timeLeftThree = 302400; 
 
         function timeIt(){
 
-            counter++; 
-            console.log(timeLeft - counter); 
+            counterThree++; 
+            console.log(timeLeftThree - counterThree); 
+
+    // if timeleft hit 0, call for counter to start again. Give circle new class
+            if (counterThree == timeLeftThree){
+
+                counterTest = 0; 
+
+                var addclassWrapper = document.querySelector('.timeToWaterThreeNone');
+                    addclassWrapper.classList.remove('timeToWaterThreeNone');
+                    addclassWrapper.classList.add('timeToWaterThree');
+
+            }
     }
             
     setInterval(timeIt, 1000); 
+
+
+// Run a tester for 10 seconds to see if timer and class change works
+
+    if (t === 'test'){
+
+        var counterTest = 0; 
+        var timeLeftTest = 10; 
+    
+            function timeIt(){
+    
+                counterTest++; 
+                console.log(timeLeftTest - counterTest); 
+
+                // if timeleft hit 0, call for counter to start again. Give circle new class
+                if (counterTest ==  timeLeftTest){
+
+                    counterTest = 0; 
+
+                    var addclassWrapper = document.querySelector('.timeToWaterThreeNone');
+                    addclassWrapper.classList.remove('timeToWaterThreeNone');
+                    addclassWrapper.classList.add('timeToWaterThree');
+
+                    document.querySelector('.startWateringThree').innerHTML = "På tide å vanne"; 
+
+                    
+                    var removeButton = document.querySelector('.startWateringThree'); 
+        
+                    removeButton.addEventListener('click', hide); 
+        
+                    function hide (event){
+        
+                    var addClass = document.querySelector('.wrapper');
+                    addClass.classList.add('hidden');
+        
+                    var addClassTwo = document.querySelector('.add'); 
+                    addClassTwo.classList.add('hidden');    
+                    
+
+                }
+        }
+                  
+        setInterval(timeIt, 1000);     
+
+    }
 }
 
+}
